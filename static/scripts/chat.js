f9b75734200ab2926ede26427db523df1caf5c22
Michelle <michellejslei@gmail.com>
2023-04-29 10:02:01 -0700

    Edit chat.js for user message function

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -13,3 +13,28 @@ for (let i = 0; i < coll.length; i++) {
         }
 });
 }
+
+// get DOM elements
+const chatbox = document.getElementById("chatbox");
+const inputBox = document.getElementById("textInput");
+
+// function to send user's message to chatbox
+function sendButton() {
+  let inputText = inputBox.value;
+  if (inputText !== "") {
+    let userHtml = '<p class="userText"><span>' + inputText + '</span></p>';
+    chatbox.insertAdjacentHTML("beforeend", userHtml);
+    inputBox.value = "";
+
+    // call function to send response from bot after a delay
+    setTimeout(function() {
+      botResponse();
+    }, 500);
+  }
+}
+
+// function to generate a response from the bot
+function botResponse() {
+  let botHtml = '<p class="botText"><span>One moment please.</span></p>';
+  chatbox.insertAdjacentHTML("beforeend", botHtml);
+}
